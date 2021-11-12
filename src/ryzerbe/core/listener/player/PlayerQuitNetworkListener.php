d3129c1cc031717fbc3594a1d8964d29953e5f6f
Baubo-LP <53007287+Baubo-LP@users.noreply.github.com>
2021-11-12 11:34:19 +0100

    party leave/delete if player quit

---
+++ src/ryzerbe/core/listener/player/PlayerQuitNetworkListener.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ryzerbe\core\listener\player;
+
+use BauboLP\Cloud\Events\PlayerQuitNetworkEvent;
+use mysqli;
+use pocketmine\event\Listener;
+use ryzerbe\core\provider\PartyProvider;
+use ryzerbe\core\util\async\AsyncExecutor;
+
+class PlayerQuitNetworkListener implements Listener {
+
+    /**
+     * @param PlayerQuitNetworkEvent $event
+     */
+    public function quit(PlayerQuitNetworkEvent $event){
+        $playerName = $event->getPlayerName();
+       AsyncExecutor::submitMySQLAsyncTask("RyZerCore", function(mysqli $mysqli) use ($playerName): void{
+           $party = PartyProvider::getPartyByPlayer($mysqli, $playerName);
+           if($party === null) return;
+           if($party === $playerName) {
+               PartyProvider::deleteParty($mysqli, $playerName);
+           }else {
+               PartyProvider::leaveParty($mysqli, $playerName, $party);
+           }
+       });
+    }
+}
