8e29187b72cc74256f20100cba887a35d3589c7b
Baubo-LP <53007287+Baubo-LP@users.noreply.github.com>
2021-11-15 15:10:35 +0100

    yeah im stupid

--- src/ryzerbe/core/provider/CoinProvider.php
+++ src/ryzerbe/core/provider/CoinProvider.php
@@ -12,14 +12,15 @@ use ryzerbe\core\RyZerBE;
 use ryzerbe\core\util\async\AsyncExecutor;
 
 class CoinProvider implements RyZerProvider {
+
     public static function addCoins(string $playerName, int $coins){
-        AsyncExecutor::submitMySQLAsyncTask("RyzerCore", function(mysqli $mysqli) use ($playerName, $coins){
+        AsyncExecutor::submitMySQLAsyncTask("RyZerCore", function(mysqli $mysqli) use ($playerName, $coins){
             $mysqli->query("UPDATE Coins SET coins=coins+'$coins' WHERE playername='$playerName'");
         }, function(Server $server, $result) use ($playerName, $coins){
             $ryzerPlayer = RyZerPlayerProvider::getRyzerPlayer($playerName);
             if($ryzerPlayer === null) return;
 
-            $ryzerPlayer->addCoins($coins, true);
+            $ryzerPlayer->addCoins($coins);
             $ryzerPlayer->getPlayer()->sendMessage(RyZerBE::PREFIX.LanguageProvider::getMessageContainer('added-coins', $ryzerPlayer->getPlayer()->getName(), ['#coins' => $coins." Coins"]));
             $ev = new PlayerCoinsAddEvent($ryzerPlayer->getPlayer(), $coins);
             $ev->call();
@@ -27,13 +28,13 @@ class CoinProvider implements RyZerProvider {
     }
 
     public static function removeCoins(string $playerName, int $coins){
-        AsyncExecutor::submitMySQLAsyncTask("RyzerCore", function(mysqli $mysqli) use ($playerName, $coins){
+        AsyncExecutor::submitMySQLAsyncTask("RyZerCore", function(mysqli $mysqli) use ($playerName, $coins){
             $mysqli->query("UPDATE Coins SET coins=coins-'$coins' WHERE playername='$playerName'");
         }, function(Server $server, $result) use ($playerName, $coins){
             $ryzerPlayer = RyZerPlayerProvider::getRyzerPlayer($playerName);
             if($ryzerPlayer === null) return;
 
-            $ryzerPlayer->removeCoins($coins, true);
+            $ryzerPlayer->removeCoins($coins);
             $ryzerPlayer->getPlayer()->sendMessage(RyZerBE::PREFIX.LanguageProvider::getMessageContainer('removed-coins', $ryzerPlayer->getPlayer()->getName(), ['#coins' => $coins." Coins"]));
             $ev = new PlayerCoinsRemoveEvent($ryzerPlayer->getPlayer(), $coins);
             $ev->call();
@@ -41,13 +42,13 @@ class CoinProvider implements RyZerProvider {
     }
 
     public static function setCoins(string $playerName, int $coins){
-        AsyncExecutor::submitMySQLAsyncTask("RyzerCore", function(mysqli $mysqli) use ($playerName, $coins){
+        AsyncExecutor::submitMySQLAsyncTask("RyZerCore", function(mysqli $mysqli) use ($playerName, $coins){
             $mysqli->query("UPDATE Coins SET coins='$coins' WHERE playername='$playerName'");
         }, function(Server $server, $result) use ($playerName, $coins){
             $ryzerPlayer = RyZerPlayerProvider::getRyzerPlayer($playerName);
             if($ryzerPlayer === null) return;
 
-            $ryzerPlayer->setCoins($coins, true);
+            $ryzerPlayer->setCoins($coins);
             $ryzerPlayer->getPlayer()->sendMessage(RyZerBE::PREFIX.LanguageProvider::getMessageContainer('set-coins', $ryzerPlayer->getPlayer()->getName(), ['#coins' => $coins." Coins"]));
         });
     }
