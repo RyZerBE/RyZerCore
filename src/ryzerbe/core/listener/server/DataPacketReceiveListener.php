e17e765fd89f1b27bcfa4c57e8512760e1f305d3
Baubo-LP <53007287+Baubo-LP@users.noreply.github.com>
2021-11-28 19:00:40 +0100

    inv bug fix (not tested)

--- src/ryzerbe/core/listener/server/DataPacketReceiveListener.php
+++ src/ryzerbe/core/listener/server/DataPacketReceiveListener.php
@@ -4,15 +4,32 @@ namespace ryzerbe\core\listener\server;
 
 use pocketmine\event\Listener;
 use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\event\server\DataPacketSendEvent;
+use pocketmine\network\mcpe\protocol\ContainerClosePacket;
 use pocketmine\network\mcpe\protocol\LoginPacket;
 use ryzerbe\core\player\data\LoginPlayerData;
 use ryzerbe\core\player\RyZerPlayerProvider;
 
 class DataPacketReceiveListener implements Listener {
+
+    private bool $cancel_send = true;
+
     public function receive(DataPacketReceiveEvent $event){
         $packet = $event->getPacket();
         if($packet instanceof LoginPacket) {
             RyZerPlayerProvider::$loginData[$packet->username] = new LoginPlayerData($packet);
+        }
+
+        if($packet instanceof ContainerClosePacket){
+            $this->cancel_send = false;
+            $event->getPlayer()->sendDataPacket($event->getPacket(), false, true);
+            $this->cancel_send = true;
+        }
+    }
+
+    public function onDataPacketSend(DataPacketSendEvent $event) : void{
+        if($this->cancel_send && $event->getPacket() instanceof ContainerClosePacket){
+            $event->setCancelled();
         }
     }
 }
