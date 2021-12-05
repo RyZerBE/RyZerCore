eb2a626b174219664ba7af11f34930f65d94a76a
Baubo-LP <53007287+Baubo-LP@users.noreply.github.com>
2021-12-05 18:04:24 +0100

    incorrect skindata fix

--- src/ryzerbe/core/listener/player/PlayerSkinChangeListener.php
+++ src/ryzerbe/core/listener/player/PlayerSkinChangeListener.php
@@ -3,13 +3,17 @@
 namespace ryzerbe\core\listener\player;
 
 use mysqli;
+use pocketmine\entity\Skin;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerChangeSkinEvent;
+use pocketmine\utils\TextFormat;
 use ryzerbe\core\language\LanguageProvider;
 use ryzerbe\core\player\PMMPPlayer;
 use ryzerbe\core\provider\PlayerSkinProvider;
 use ryzerbe\core\RyZerBE;
 use ryzerbe\core\util\async\AsyncExecutor;
+use ryzerbe\core\util\skin\SkinDatabase;
+use function strlen;
 
 class PlayerSkinChangeListener implements Listener {
 
@@ -25,6 +29,16 @@ class PlayerSkinChangeListener implements Listener {
         if($player->hasDelay("skin_change")){
             $event->setCancelled();
             $player->sendMessage(RyZerBE::PREFIX.LanguageProvider::getMessageContainer("skin-change-cancel-delay", $player));
+            return;
+        }
+
+        $correct_size = Skin::ACCEPTED_SKIN_SIZES[2];
+        if(strlen($skinData) > $correct_size) {
+            SkinDatabase::getInstance()->loadSkin("steve", function(bool $success) use ($player): void{
+                if(!$player->isConnected() || !$player instanceof PMMPPlayer) return;
+                if(!$success) $player->kickFromProxy("&cSkin aren't allowed! Please switch your skin!");
+                else $player->sendMessage(RyZerBE::PREFIX.TextFormat::RED."Skin aren't allowed!");
+            }, null, $player);
             return;
         }
 
