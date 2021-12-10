5c062a9aacc83bcc9faee4f624f55729cee1093c
Baubo-LP <53007287+Baubo-LP@users.noreply.github.com>
2021-12-10 20:16:31 +0100

    knock improvement (i hope)

--- src/ryzerbe/core/listener/entity/EntityDamageByEntityListener.php
+++ src/ryzerbe/core/listener/entity/EntityDamageByEntityListener.php
@@ -47,6 +47,7 @@ class EntityDamageByEntityListener implements Listener {
             $item = $player->getInventory()->getItemInHand();
             if($item->hasEnchantment(Enchantment::KNOCKBACK) && !$event->isCancelled()){
                 $event->setCancelled();
+                $entity->setSprinting(false);
                 $entity->broadcastEntityEvent(ActorEventPacket::HURT_ANIMATION);
                 $this->knockback($entity, $player, $item->getEnchantmentLevel(Enchantment::KNOCKBACK));
                 $entity->setHealth($entity->getHealth() - $event->getFinalDamage());
