e4a961e1d0b775746a7cfcd6c3a4982d1f7898e0
Baubo-LP <53007287+Baubo-LP@users.noreply.github.com>
2021-12-18 13:57:30 +0100

    knockback improvements

--- src/ryzerbe/core/listener/entity/EntityDamageByEntityListener.php
+++ src/ryzerbe/core/listener/entity/EntityDamageByEntityListener.php
@@ -2,18 +2,25 @@
 
 namespace ryzerbe\core\listener\entity;
 
+use pocketmine\block\BlockIds;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\Listener;
 use pocketmine\item\enchantment\Enchantment;
+use pocketmine\item\ItemIds;
 use pocketmine\math\Vector3;
 use pocketmine\network\mcpe\protocol\ActorEventPacket;
 use pocketmine\network\mcpe\protocol\AnimatePacket;
 use ryzerbe\core\player\PMMPPlayer;
 use ryzerbe\core\util\Settings;
+use function microtime;
 
 class EntityDamageByEntityListener implements Listener {
     private array $delay = [];
+
+    const KNOCK_HAND = 0;
+    const KNOCK_SWORD = 1;
+    const KNOCK_STICK = 2;
 
     /**
      * @priority HIGH
@@ -26,7 +33,7 @@ class EntityDamageByEntityListener implements Listener {
         $ryzerPlayer = $player->getRyZerPlayer();
         if($ryzerPlayer === null) return;
 
-        if($ryzerPlayer->getPlayerSettings()->isMoreParticleActivated()) {
+        if($ryzerPlayer->getPlayerSettings()->isMoreParticleActivated()){
             $pk = new AnimatePacket();
             $pk->action = AnimatePacket::ACTION_CRITICAL_HIT;
             $pk->entityRuntimeId = $entity->getId();
@@ -42,48 +49,72 @@ class EntityDamageByEntityListener implements Listener {
             return;
         }
         $event->setModifier(0, EntityDamageEvent::MODIFIER_TOTEM);
-        $this->delay[$player->getName()] = microtime(true) + 0.25;
         if($entity instanceof PMMPPlayer){
             $item = $player->getInventory()->getItemInHand();
-            if($item->hasEnchantment(Enchantment::KNOCKBACK) && !$event->isCancelled()){
+            if($event->isCancelled()) return;
+            if($player->distance($entity) > 4 && $item->getId() === ItemIds::STICK){
+                $event->setCancelled();
+                return;
+            }
+            if($item->hasEnchantment(Enchantment::KNOCKBACK)){
+                $this->delay[$player->getName()] = microtime(true) + 0.35;
                 $event->setCancelled();
                 $entity->setImmobile(true);
                 $entity->setImmobile(false);
                 $entity->broadcastEntityEvent(ActorEventPacket::HURT_ANIMATION);
-                $this->knockback($entity, $player, $item->getEnchantmentLevel(Enchantment::KNOCKBACK));
+                $this->knockback($entity, $player, self::KNOCK_STICK);
                 $entity->setHealth($entity->getHealth() - $event->getFinalDamage());
                 //thats here bc I think so its maybe fewer player resistance
+            }elseif($item->getId() != ItemIds::STICK && $item->getId() != ItemIds::GOLDEN_SWORD && $item->getId() != ItemIds::WOODEN_SWORD){
+                $this->delay[$player->getName()] = microtime(true) + 0.35;
+                $event->setCancelled();
+                $entity->setImmobile(true);
+                $entity->setImmobile(false);
+                $entity->broadcastEntityEvent(ActorEventPacket::HURT_ANIMATION);
+                $this->knockback($entity, $player, self::KNOCK_HAND);
+                $entity->setHealth($entity->getHealth() - $event->getFinalDamage());
             }
         }
     }
 
     public function knockback(PMMPPlayer $entity, PMMPPlayer $attacker, int $level = 1): void{
-        if($level === 0) return;
         $motion = clone $entity->getMotion();
         $motion->y /= 2;
-        $motion->y += 0.45;
+        $motion->y += ($level === self::KNOCK_HAND) ? 0.35 : 0.5;
         if($motion->y > 0.45)
             $motion->y = 0.45;
 
-        if(!$entity->useLadder()){
-            $knockBack = 1.1;
-            if($level > 1)
-                $knockBack = 0.9;
+        switch($level){
+            case self::KNOCK_HAND:
+                $knockBack = 2.5;
+                if($attacker->getPitch() >= 70){
+                    $motion = new Vector3($attacker->getLookVector(0.48)->x / $knockBack, $motion->y, $attacker->getLookVector(0.48)->z / $knockBack);
+                }else{
+                    $motion = new Vector3($attacker->getLookVector()->x / $knockBack, $motion->y, $attacker->getLookVector()->z / $knockBack);
+                }
 
-            if($attacker->getPitch() >= 70) {
-                $motion = new Vector3($attacker->getLookVector(0.48)->x / $knockBack, $motion->y, $attacker->getLookVector(0.48)->z / $knockBack);
-            }else {
-                $motion = new Vector3($attacker->getLookVector()->x / $knockBack, $motion->y, $attacker->getLookVector()->z / $knockBack);
-            }
+                $entity->setMotion($motion);
+                break;
+            case self::KNOCK_STICK:
+                if(!$entity->useLadder()){
+                    $knockBack = 1.2;
+
+                    if($attacker->getPitch() >= 70){
+                        $motion = new Vector3($attacker->getLookVector(0.48)->x / $knockBack, $motion->y, $attacker->getLookVector(0.48)->z / $knockBack);
+                    }else{
+                        $motion = new Vector3($attacker->getLookVector()->x / $knockBack, $motion->y, $attacker->getLookVector()->z / $knockBack);
+                    }
+                }
+
+                if(Settings::$reduce){
+                    $ownmotion = $attacker->getMotion();
+                    $ownmotion->setComponents($ownmotion->getX() * 0.6, $ownmotion->getY() * 0.6, $ownmotion->getZ() * 0.6);
+                    $attacker->setMotion($ownmotion);
+                    $attacker->setSprinting(false);
+                }
+
+                $entity->setMotion($motion);
+                break;
         }
-
-        if(Settings::$reduce){
-            $ownmotion = $attacker->getMotion();
-            $ownmotion->setComponents($ownmotion->getX() * 0.6, $ownmotion->getY() * 0.6, $ownmotion->getZ() * 0.6);
-            $attacker->setMotion($ownmotion);
-            $attacker->setSprinting(false);
-        }
-
-        $entity->setMotion($motion);
     }
 }
