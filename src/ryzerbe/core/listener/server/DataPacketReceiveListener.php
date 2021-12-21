61003e517f419ce678189a7e6d02aad3a9f39693
Baubo-LP <53007287+Baubo-LP@users.noreply.github.com>
2021-12-21 20:13:27 +0100

    player emotes fixed

--- src/ryzerbe/core/listener/server/DataPacketReceiveListener.php
+++ src/ryzerbe/core/listener/server/DataPacketReceiveListener.php
@@ -6,7 +6,9 @@ use pocketmine\event\Listener;
 use pocketmine\event\server\DataPacketReceiveEvent;
 use pocketmine\event\server\DataPacketSendEvent;
 use pocketmine\network\mcpe\protocol\ContainerClosePacket;
+use pocketmine\network\mcpe\protocol\EmotePacket;
 use pocketmine\network\mcpe\protocol\LoginPacket;
+use pocketmine\Server;
 use ryzerbe\core\player\data\LoginPlayerData;
 use ryzerbe\core\player\RyZerPlayerProvider;
 
@@ -24,6 +26,10 @@ class DataPacketReceiveListener implements Listener {
             $this->cancel_send = false;
             $event->getPlayer()->sendDataPacket($event->getPacket(), false, true);
             $this->cancel_send = true;
+        }
+        if($packet instanceof EmotePacket){
+            $emoteId = $packet->getEmoteId();
+            Server::getInstance()->broadcastPacket($event->getPlayer()->getViewers(), EmotePacket::create($event->getPlayer()->getId(), $emoteId, 1 << 0));
         }
     }
 
