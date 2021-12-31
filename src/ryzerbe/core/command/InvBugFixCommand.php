62f6212bfeb39567f446f90684ba097c229f34a2
Baubo-LP <53007287+Baubo-LP@users.noreply.github.com>
2021-12-31 15:33:37 +0100

    maybe invbug fix

--- src/ryzerbe/core/command/InvBugFixCommand.php
+++ src/ryzerbe/core/command/InvBugFixCommand.php
@@ -5,7 +5,9 @@ namespace ryzerbe\core\command;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\network\mcpe\protocol\ChangeDimensionPacket;
+use pocketmine\network\mcpe\protocol\ContainerClosePacket;
 use pocketmine\network\mcpe\protocol\PlayStatusPacket;
+use pocketmine\network\mcpe\protocol\types\ContainerIds;
 use pocketmine\network\mcpe\protocol\types\DimensionIds;
 use pocketmine\utils\TextFormat;
 use ryzerbe\core\player\PMMPPlayer;
@@ -26,24 +28,13 @@ class InvBugFixCommand extends Command {
      */
     public function execute(CommandSender $sender, string $commandLabel, array $args){
         if(!$sender instanceof PMMPPlayer) return;
-        if(!$sender->isOp()) {
-            $sender->sendMessage(TextFormat::RED."Dieser Command funktioniert leider noch nicht :(");
-            $sender->sendMessage(TextFormat::RED."Bitte joine dem Netzwerk neu, falls du den Invbug hast.");
-            return;
-        }
-        $position = $sender->asPosition();
-        if(!$sender->hasDelay("change_dimension")){
-            $sender->teleport($position);
-            $pk = new ChangeDimensionPacket();
-            $pk->position = $position;
-            $pk->dimension = DimensionIds::OVERWORLD;
-            $pk->respawn = true;
-            $sender->dataPacket($pk);
-            $sender->addDelay("change_dimension", 4);
-            AsyncExecutor::submitClosureTask(TaskUtils::secondsToTicks(2), function(int $currentTick) use ($sender, $position): void{
-                if(!$sender->isConnected()) return;
-                $sender->sendPlayStatus(PlayStatusPacket::PLAYER_SPAWN);
-            });
-        }
+
+        $pk = new ContainerClosePacket();
+        $pk->windowId = ContainerIds::INVENTORY;
+        $sender->dataPacket($pk);
+
+        $pk->windowId = ContainerIds::FIXED_INVENTORY;
+        $sender->dataPacket($pk);
+        $sender->sendMessage("FIX");
     }
 }
