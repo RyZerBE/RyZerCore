4c5a1c269eacdf0e275d5e881cdc386d5619cd42
Baubo-LP <53007287+Baubo-LP@users.noreply.github.com>
2022-01-01 15:10:04 +0100

    removed java rod code

--- src/ryzerbe/core/item/rod/entity/FishingHook.php
+++ src/ryzerbe/core/item/rod/entity/FishingHook.php
@@ -6,13 +6,11 @@ use pocketmine\entity\Entity;
 use pocketmine\entity\projectile\Projectile;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\entity\EntityDamageEvent;
-use pocketmine\item\FishingRod;
 use pocketmine\level\Level;
 use pocketmine\math\RayTraceResult;
 use pocketmine\math\Vector3;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\network\mcpe\protocol\ActorEventPacket;
-use pocketmine\utils\Random;
 use ryzerbe\core\item\rod\PvPRod;
 use ryzerbe\core\player\PMMPPlayer;
 use function sqrt;
@@ -46,40 +44,6 @@ class FishingHook extends Projectile {
         return 1;
     }
 
-    public function throwHookJava(): void{
-        $entity = $this->getOwningEntity();
-        if($entity === null) return;
-
-        $location = $entity->getLocation();
-        $f = $location->pitch;
-        $f1 = $location->yaw;
-        $f2 = cos(deg2rad(-$f1) - M_PI);
-        $f3 = sin(deg2rad(-$f1) - M_PI);
-        $f4 = -cos(deg2rad(-$f));
-        $f5 = sin(deg2rad(-$f));
-        $d0 = $location->x - $f3 * 0.3;
-        $d1 = $location->y + $entity->getEyeHeight();
-        $d2 = $location->z - $f2 * 0.3;
-        $this->setPositionAndRotation(new Vector3($d0, $d1, $d2), $f1, $f);
-        $vec3 = new Vector3(
-            -$f3,
-            max(-5.0, min(5.0, -($f5 / $f4))),
-            -$f2
-        );
-        $d3 = $vec3->length();
-        $random = new Random();
-        $vec3 = new Vector3(
-            $vec3->x * (0.6 / $d3 + 0.5 + $random->nextFloat() * 0.0045),
-            $vec3->y * (0.6 / $d3 + 0.5 + $random->nextFloat() * 0.0045),
-            $vec3->z * (0.6 / $d3 + 0.5 + $random->nextFloat() * 0.0045)
-        );
-        $this->setMotion($vec3);
-        $this->setRotation(
-            rad2deg(atan2($vec3->x, $vec3->z)),
-            rad2deg(atan2($vec3->y, sqrt($vec3->x * $vec3->x + $vec3->z * $vec3->z)))
-        );
-    }
-
     public function onUpdate(int $currentTick): bool{
         if($this->isFlaggedForDespawn() || !$this->isAlive()){
             return false;
@@ -101,7 +65,6 @@ class FishingHook extends Projectile {
         return $hasUpdate;
     }
 
-
     public function throwHook(PvPRod $fishingRod){
        $this->setMotion($this->getMotion()->multiply($fishingRod->getThrowForce()));
     }
@@ -116,8 +79,6 @@ class FishingHook extends Projectile {
         $d8 = 0.1;
         $this->setMotion(new Vector3($d0 * $d8, $d2 * $d8 + sqrt($d6) * 0.08, $d4 * $d8));
     }
-    
-    
 
     protected function onHitEntity(Entity $entityHit, RayTraceResult $hitResult): void{
         $player = $this->getOwningEntity();
