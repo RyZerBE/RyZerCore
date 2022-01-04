ea193446dc70e93ef931a7985f1235b88b4746da
Baubo-LP <53007287+Baubo-LP@users.noreply.github.com>
2022-01-04 18:27:05 +0100

    reduced knock power bow

--- src/ryzerbe/core/entity/Arrow.php
+++ src/ryzerbe/core/entity/Arrow.php
@@ -135,7 +135,7 @@ class Arrow extends Projectile {
     protected function onHitEntity(Entity $entityHit, RayTraceResult $hitResult) : void{
         parent::onHitEntity($entityHit, $hitResult);
         $horizontalSpeed = sqrt($this->motion->x ** 2 + $this->motion->z ** 2);
-        if($this->punchKnockback <= 0) $this->punchKnockback = 1;
+        if($this->punchKnockback <= 0) $this->punchKnockback = 0.5;
         if($horizontalSpeed > 0){
             $multiplier = $this->punchKnockback * 0.5 / $horizontalSpeed;
             $entityHit->setMotion($entityHit->getMotion()->add($this->motion->x * $multiplier, 0.1, $this->motion->z * $multiplier));
