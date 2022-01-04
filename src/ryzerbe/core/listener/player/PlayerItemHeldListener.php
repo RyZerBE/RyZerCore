add78cf53387c30fe0530dd95d684ebdea31aa04
Baubo-LP <53007287+Baubo-LP@users.noreply.github.com>
2022-01-04 16:13:52 +0100

    despawn pvp fishing hook if item switch

--- src/ryzerbe/core/listener/player/PlayerItemHeldListener.php
+++ src/ryzerbe/core/listener/player/PlayerItemHeldListener.php
@@ -7,10 +7,12 @@ namespace ryzerbe\core\listener\player;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerItemHeldEvent;
 use pocketmine\item\Shield;
+use ryzerbe\core\player\PMMPPlayer;
 
 class PlayerItemHeldListener implements Listener {
     public function onPlayerItemHeld(PlayerItemHeldEvent $event): void {
         $player = $event->getPlayer();
+        if(!$player instanceof PMMPPlayer) return;
         $item = $event->getItem();
 
         if($item instanceof Shield) {
@@ -18,5 +20,8 @@ class PlayerItemHeldListener implements Listener {
             $player->setSneaking(!$player->isSneaking());
             $player->setSneaking(!$player->isSneaking());
         }
+
+        if($player->getPvpFishingHook() === null) return;
+        $player->getPvpFishingHook()->flagForDespawn();
     }
 }
