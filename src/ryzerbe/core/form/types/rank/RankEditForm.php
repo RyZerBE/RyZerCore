6a15e1c7884d907771f24be46191834831c2fb8f
Baubo-LP <53007287+Baubo-LP@users.noreply.github.com>
2022-03-03 15:39:20 +0100

    show permissions to rank ui form

--- src/ryzerbe/core/form/types/rank/RankEditForm.php
+++ src/ryzerbe/core/form/types/rank/RankEditForm.php
@@ -6,6 +6,7 @@ use jojoe77777\FormAPI\SimpleForm;
 use pocketmine\Player;
 use pocketmine\utils\TextFormat;
 use ryzerbe\core\rank\Rank;
+use function implode;
 
 class RankEditForm {
     public static function onOpen(Player $player, array $extraData = []): void{
@@ -30,6 +31,7 @@ class RankEditForm {
                     break;
             }
         });
+        $form->setContent(implode("\n", $rank->getPermissions()));
         $form->addButton(TextFormat::LIGHT_PURPLE . "Design", 1, "https://media.discordapp.net/attachments/602115215307309066/907567439917744139/1454971.png?width=410&height=410", "design");
         $form->addButton(TextFormat::AQUA . "JoinPower", 1, "https://media.discordapp.net/attachments/602115215307309066/907571028790755348/1f4aa-1f3fc.png?width=410&height=410", "join_power");
         $form->addButton(TextFormat::GREEN . "Add Permission", 1, "https://media.discordapp.net/attachments/602115215307309066/907567888418869248/2172839.png?width=410&height=410", "add_permission");
