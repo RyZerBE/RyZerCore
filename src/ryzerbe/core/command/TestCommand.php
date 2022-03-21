76ebb6515c896d234bf44f7876160b3232f777bc
Matze <matthias.kuegele@gmail.com>
2022-03-21 17:10:52 +0100

    TestCommand: Add permission

--- src/ryzerbe/core/command/TestCommand.php
+++ src/ryzerbe/core/command/TestCommand.php
@@ -5,22 +5,18 @@ namespace ryzerbe\core\command;
 use jojoe77777\FormAPI\SimpleForm;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\level\Position;
 use pocketmine\Player;
-use pocketmine\utils\TextFormat;
-use ryzerbe\core\entity\SkullEntity;
-use ryzerbe\core\util\animation\AnimationManager;
-use ryzerbe\core\util\animation\defaults\FireworkPlayerHeadWinnerAnimation;
 use ryzerbe\core\util\SkinUtils;
 
 class TestCommand extends Command {
 
     public function __construct(){
         parent::__construct("test", "player head test", "", []);
+        $this->setPermission("ryzer.test");
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args){
-        if(!$sender instanceof Player) return;
+        if(!$sender instanceof Player || !$this->testPermission($sender)) return;
 
         $form = new SimpleForm(function(Player $player, $data): void{});
         $form->addButton($sender->getName()." Head", 1, SkinUtils::getPlayerHeadIcon($sender->getName()));
